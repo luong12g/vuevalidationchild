cd00e9be835e6d14239ef5c6d1577ec261c0ac48
luong12g <tung@12g.co>
2022-02-08 09:11:15 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <h1>Child Component validation with Vuelidate</h1>
+    <form-component />
+  </div>
+</template>
+
+<script>
+import FormComponent from "./components/FormComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    FormComponent
+  }
+};
+</script>
